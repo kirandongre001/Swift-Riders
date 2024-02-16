72b4fa1df7268737934e328abd27ead63be46ec5
kiran dongre <kirandongre001@gmail.com>
2024-02-16 21:13:42 +0530

    Webapp Program file

--- Swift-Rides/SwiftRidesDOTNET/Program.cs
+++ Swift-Rides/SwiftRidesDOTNET/Program.cs
@@ -1,44 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace SwiftRides
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+ public class Program
+ {
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    // Add services to the container.
+    builder.Services.AddControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    builder.Services.AddSwaggerGen();
 
-app.Run();
+    builder.Services.AddDbContext<CarpoolprojectContext>();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
+    builder.Services.AddCors(policybuilder => policybuilder.AddDefaultPolicy(policy =>
+       policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())) ;
+
+    var app = builder.Build();
+
+    app.UseCors();
+    
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+  }
+ }
 }
